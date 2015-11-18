6496f0043f007ce0d9d80023ef44a835728b5d37
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-11-18 23:20:58 +0100

    * added "event" model

--- src/aping-models.js
+++ src/aping-models.js
@@ -65,6 +65,31 @@ apingApp.service('apingModels', [function () {
                 };
                 break;
 
+            case "event":
+                model = {
+                    platform : _platform, //NAME of platform ( "youtube" / "facebook", "instagram" , ...)
+                    blog_name : false, //NAME of blog (channel / facebook page, instagram account, ..)
+                    blog_id : false, //ID of channel / page / account, ...
+                    blog_link : false, //link to channel / uploader / page / account, ...
+                    start_timestamp : false,
+                    end_timestamp: false,
+                    event_url : false, //URL to the event
+                    ticket_url : false, //URL to the ticket
+                    intern_id : false, // INTERN ID of event (facebook id, instagram id, ...)
+                    text : false,
+                    caption : false,
+                    img_url : false,
+                    place_name : false,
+                    city: false,
+                    country: false,
+                    latitude: false,
+                    longitude : false,
+                    street: false,
+                    zip : false,
+                    source : false,
+                };
+                break;
+
             default:
                 break;
         }
