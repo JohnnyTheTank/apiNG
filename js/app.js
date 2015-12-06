a966e22d606cf005c7b64e528fdf903fa5fa9bf7
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-06 13:34:13 +0100

    demo: updated view

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('app', [
+    'jtt_aping',
+    'jtt_aping_youtube',
+    'jtt_aping_instagram',
+    'jtt_aping_facebook',
+    'jtt_aping_codebird',
+    'jtt_aping_vimeo',
+    'jtt_aping_design_default',
+    'hljs'
+]);
+
+app.config(function (hljsServiceProvider) {
+    hljsServiceProvider.setOptions({
+        fixMarkup: false
+    });
+});
