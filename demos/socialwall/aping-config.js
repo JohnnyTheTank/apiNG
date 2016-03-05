f4e2b44dd68644a2613512241c5adb35258f69fc
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-03-05 09:28:26 +0100

    small update

--- demos/socialwall/aping-config.js
+++ demos/socialwall/aping-config.js
@@ -20,12 +20,12 @@ angular.module('jtt_aping').config(['$provide', function ($provide) {
             ],
             facebook: [
                 {'access_token': 'CAAMC6AyvDH0BAChyXyiSXINl6nAZCaOP0nXS1T67ngaZA0svefRAJyWx3Y5bzic47wE4iZAMHCAAoQCvhUMZB3SQufDyio9g4vZAj5BgQXuMVDjVvqDrajfm7IEvN0U1O2JfC1FA2VfqpnYgRa9fF5ZAQBW4vHc6ZAmXryyJvzPQd5TkIdqUC4n'},
-                {'access_token': '1739743719594435|trN6PJoujEA5y0yjj-7xJYSKZYw'}
+                {'access_token': '1739743719594435|trN6PJoujEA5y0yjj-7xJYSKZYw'},
             ],
             twitter: [
                 {'bearer_token': 'AAAAAAAAAAAAAAAAAAAAACs3iwAAAAAA%2BH8LdT7YfKIzk8fvQvqKvoePuxo%3DMKfXX2ojtacGL8aOLAhlBDUrEFpy0DUuiyebet22Wa2YVrX7cc'},
                 {'bearer_token': 'AAAAAAAAAAAAAAAAAAAAAMzktwAAAAAADOjiwPVsXlzKQSo4s%2FHGJBOgFLY%3DKT4isgacAM2doH8WHFmyYOJ58x4M95WVrrtA3b9pEsWBQJRNtQ'},
-                {'bearer_token': 'AAAAAAAAAAAAAAAAAAAAALohjgAAAAAA2I2NKaljOFOCVvI0wxF6pcL6jMY%3D5dt8TBHusrHQiG7SntIsIYoiSqQxTDmiadTsyjNsSDOMpb9wwA'}
+                {'bearer_token': 'AAAAAAAAAAAAAAAAAAAAALohjgAAAAAA2I2NKaljOFOCVvI0wxF6pcL6jMY%3D5dt8TBHusrHQiG7SntIsIYoiSqQxTDmiadTsyjNsSDOMpb9wwA'},
             ],
             vimeo: [
                 {'access_token': '1e53b10910c18adf97682d12e01e60d6'},
